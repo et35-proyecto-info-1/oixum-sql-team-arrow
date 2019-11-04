3851c4b9a9e5ddca0ed774e97ae2ada2c477d084
franmendozita123@gmail.com <franmendozita123@gmail.com>
2019-11-04 17:41:22 -0300

    consulta e imagen

--- src/app.js
+++ src/app.js
@@ -4,31 +4,35 @@ var exphbs = require('express-handlebars');
 
 var path = require('path');
 
-
 var app = express();
 
+app.use(express.static('/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.render('home', {
-    mensaje: '¡Hola ET 35!',
-    texto: 'Esto también sale del servidor',
-    fecha: new Date()
-  });
-  });
+  res.render('home');
+});
 
 function boton(){
 var tConsulta=text(document.getElementById("consulta").value)
 }
 
-app.get('/query', function(req, res) {
-  connection.query(tConsulta, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
+app.post('/', function(req, res) {
+  const query = req.body.query;
+  connection.query(query, function (error, results, fields) {
+    res.render('home', {
+      resultados: JSON.stringify(results),
+      columnas: JSON.stringify(fields),
+      error: error
+    });
   });
 });
+
+
 
 app.listen(3000, function() {
   connection.connect();
